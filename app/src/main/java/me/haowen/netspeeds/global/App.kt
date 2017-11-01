4558a7a54773979aa716acbea1143b6588df0455
haowen <haowenhello@gmial.com>
2017-11-01 11:11:54 +0800

    Add KDoc plugin dokka

--- app/src/main/java/me/haowen/netspeeds/global/App.kt
+++ app/src/main/java/me/haowen/netspeeds/global/App.kt
@@ -5,11 +5,13 @@ import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
 import com.facebook.stetho.Stetho
+import me.haowen.netspeeds.global.App.Companion.context
 
 /**
- * author: yyhy
- * date  : 2017/10/10 17:44
- * desc  : App
+ * The Application of this project.
+ *
+ * @author haowen
+ * @property context context of base Application
  */
 class App : Application() {
 
@@ -19,6 +21,9 @@ class App : Application() {
         lateinit var sharePre: SharedPreferences
     }
 
+    /**
+     * When Application is created, It will be called.
+     */
     override fun onCreate() {
         super.onCreate()
         context = this
